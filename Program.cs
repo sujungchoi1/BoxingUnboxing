eeee9615c2ab8671686ffd6882400ad21e2de1a8
Sujung Choi <schoi0210@gmail.com>
2021-02-15 20:00:10 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<object> newObj = new List<object>();
+            newObj.Add(7);
+            newObj.Add(28);
+            newObj.Add(-1);
+            newObj.Add(true);
+            newObj.Add("chair");
+
+            // iterate using type inference
+            foreach (var entry in newObj)
+            {
+                Console.WriteLine(entry);
+            }
+            
+            // Add all values that are Int type together and output the sum
+            int sum = 0;
+            foreach (var entry in newObj)
+            {
+                if (entry is int)
+                {
+                    sum = sum + (int)entry;
+                }
+            }
+            Console.WriteLine(sum);
+        }   
+
+    }
+}
+
